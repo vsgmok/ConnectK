e246597a14786ebc46edaaafa1d02aed919974e5
Vikram Sugumaran <vsugumar@uci.edu>
2016-12-04 12:25:17 -0800

    all parts

--- AIShell.h
+++ AIShell.h
@@ -67,7 +67,7 @@ public:
 	int deadline; //this is how many milliseconds the AI has to make move.
 	int k;        // k is the number of pieces a player must get in a row/column/diagonal to win the game. IE in connect 4, this variable would be 4
 
-	AIShell(int numCols, int numRows, bool gravityOn, int** gameState, Move lastMove, Move move);
+	AIShell(int numCols, int numRows, bool gravityOn, int** gameState, Move move);
 	~AIShell();
 	Move makeMove();
 };
